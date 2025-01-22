54f838ac8a964b94962f1f9cfab2f9b200efc02c
Mohammad-Rakibuzzaman <rtzrakib347@gmail.com>
2025-01-23 02:47:15 +0600

    add csrf

--- hirik_bazar/settings.py
+++ hirik_bazar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://hirikbazarbd.onrender.com', 'localhost', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://hirikbazarbd.onrender.com', 'localhost', 'https://*.127.0.0.1']
 
 
 # Application definition
